db1e331c604aa5a1220b58c1ee597f57a90ce138
Aman87099 <97830224+Aman87099@users.noreply.github.com>
2023-01-30 15:57:07 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (72.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int lo = 0;
+        int hi = nums.size()-1;
+        int mid = 0;
+        
+        while(mid<=hi){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums[lo++], nums[mid++]);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid], nums[hi--]);
+                    break;
+            }
+        }
         
     }
 };
